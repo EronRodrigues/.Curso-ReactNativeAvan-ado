bdcf0b157609b70e966fbdc442faaff56fbbc5ef
Eron Rodrigues <109868628+EronRodrigues@users.noreply.github.com>
2022-12-11 14:58:50 -0300

    Delete src directory

--- src/styles/colors.ts
+++
@@ -1,8 +0,0 @@
-export const colors = {
-    background: "#181C1F",
-    text: "#8C8E8F",
-    button: "#1D2528",
-    buttonGrey: "#364A53",
-    buttonBlue: "#7FD1F6",
-    buttonPurple: "#434172",
-}
